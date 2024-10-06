64ed91b8d81fef3953ddbbb1fed799ba30154d32
Ananthu T J <ananthutj832@gmail.com>
2024-10-06 16:34:44 +0530

    Made some change in size

--- src/sidebar.js
+++ src/sidebar.js
@@ -4,31 +4,33 @@ import { auth } from "./firebase_config";
 import { useNavigate } from "react-router-dom";
 import "./sidebar.css";
 
-const Sidebar = forwardRef(({ uid, name , isAdmin}, ref) => {
+const Sidebar = forwardRef(({ uid, name, isAdmin }, ref) => {
   const navigate = useNavigate();
-  
+
   const handleLogout = async () => {
     await auth.signOut();
     navigate("/");
   };
-  
+
   return (
     <div className="sidebar" ref={ref}>
       <h2>Welcome, {name}!</h2>
       <ul>
         <li>
-          <Link to={`/messcut/${uid}`}>Mess Cut</Link>
+          <Link to="/profile">Profile</Link>
         </li>
-        {isAdmin == false && (
+        <li>
+          {/* <Link to={`/messcut/${uid}`}>Mess Cut</Link> */}
+          <Link>Mess Cut</Link>
+        </li>
+        {isAdmin == true && (
           <li>
             <Link to="/allusers">All Users</Link>
           </li>
         )}
         <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li>
-          <Link to={`/bill/${uid}`}>Bill</Link>
+          {/* <Link to={`/bill/${uid}`}>Bill</Link> */}
+          <Link>Bill</Link>
         </li>
         <li>
           <button className="logout-button" onClick={handleLogout}>
