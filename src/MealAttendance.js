bd83bc95956f9cb32dbbbfbd4bdad9cded3caab2
Srishanthj <srish.jayan2@gmail.com>
2024-10-14 16:03:05 +0530

    Checkbox updatedd

--- src/MealAttendance.js
+++ src/MealAttendance.js
@@ -20,7 +20,7 @@ const MealAttendance = () => {
   const getCurrentDate = () => {
     const date = new Date();
     const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, '0'); 
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const day = date.getDate().toString().padStart(2, '0');
     return `${year}-${month}-${day}`;
   };
@@ -126,7 +126,7 @@ const MealAttendance = () => {
     try {
       console.log(currentDate)
       console.log(currentMonth)
-      
+
       await updateDoc(userRef, {
         mealAttendance: {
           0: {
